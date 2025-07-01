eda74f53a446a7ee100fbc433c589037db04b77d
Grégoire Courtois <courtoisgregoire@gmail.com>
2025-07-01 23:22:38 +0200

    trying

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,5 @@ Rails.application.config.assets.paths << Rails.root.join("node_modules")
 
 # Add Active Admin assets to the precompile list
 Rails.application.config.assets.precompile += %w[ active_admin.css active_admin.js ]
+
+Rails.application.config.assets.css_compressor = nil
