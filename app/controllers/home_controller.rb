e3948c0a108f63cb4fc682de83790297a6d1e0e2
Grégoire Courtois <courtoisgregoire@gmail.com>
2026-04-12 22:00:19 +0200

    Fix session bug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,12 @@
 class HomeController < ApplicationController
   def index
     @session = Session.current_or_upcoming
-  
-    if @session
+
+    if @session.present?
       # Récupérer programmations et événements
       @programmations = @session.programmations.includes(:movie).order(:time)
       @events = @session.events.order(:start_time)
-      
+
       # Combiner et trier par date
       @schedule_items = (@programmations.to_a + @events.to_a).sort_by do |item|
         item.is_a?(Programmation) ? item.time : item.start_time
